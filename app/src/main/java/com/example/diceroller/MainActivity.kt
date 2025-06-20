c0012e0f5cf72122927c6df94f5a3f6f0cfbbb65
haper <hapereira2004@gmail.com>
2025-06-20 13:25:18 +0100

    Adicionada lógica de lançamento de dado com estado Utilizado remember { mutableStateOf(1) } para armazenar o resultado
    Adicionada lógica para gerar número aleatório entre 1 e 6 ao clicar no botão
    
    Adicionada estrutura when para escolher imagem com base no resultado
    
    Image() atualizada com recurso e descrição dinâmicos

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -19,6 +19,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,18 +47,25 @@ fun DiceRollerApp(){
 
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
-    Column(modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription = "1"
-        )
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf( 1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
-        Text(stringResource(R.string.roll))
+
+        Button(
+            onClick = { result = (1..6).random() },
+        ) {
+            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
     }
-
 }
