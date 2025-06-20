39fcce50cc25ef9e8a38d189ddb53091bad19d63
haper <hapereira2004@gmail.com>
2025-06-20 13:03:51 +0100

    Adicionado botão "Roll" ao layout Criada string roll no strings.xml
    Adicionado botão na Column() com texto vindo de stringResource(R.string.roll)

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -12,11 +11,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DiceRollerTheme {
                 DiceRollerApp()
@@ -39,6 +40,10 @@ fun DiceRollerApp(){
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
     Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) { }
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+        Text(stringResource(R.string.roll))
+        }
+    }
 
 }
